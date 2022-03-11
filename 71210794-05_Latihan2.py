9fd77c56252dd885e33c75b9a6b972d7b403f27e
EvanderGab <evander.gabriel@ti.ukdw.ac.id>
2022-03-11 17:59:18 +0700

    Revisi latihan 2

--- 71210794-05_Latihan2.py
+++ 71210794-05_Latihan2.py
@@ -3,7 +3,7 @@ b = int(input("Masukan batas atas = "))
 
 def ganjil(a,b):
     if a > b :
-        for i in reversed(range(b,a+1)):
+        for i in range (a,b-1,-1):
             if i % 2 == 1:
                 print(i, end= " ")
     else:
